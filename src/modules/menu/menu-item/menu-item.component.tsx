e2131515e95db7061a33a8fc657683788eebe849
denisbogoslovskiy <bogoslovskiydenis@gmail.com>
2023-03-01 13:58:17 +0200

    storybook menu-Items

---
+++ src/modules/menu/menu-item/menu-item.component.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react'
+
+interface MenuItemProps{
+	imagePath: string,
+	weight: number,
+	title: string,
+	description: string,
+	price: number
+}
+
+
+const MenuItem:FC<MenuItemProps> = ({imagePath, weight,title,description,price}) => {
+	return (
+		<div className='w-96 shadow-xl rounded-2xl bg-white'>
+			<div className='relative '>
+				<img src={imagePath} alt='' className='rounded-t-2xl object-cover w-full h-[15rem] object-center' />
+				<span className='absolute  bottom-2 right-3  text-white text-sm px-2 rounded-[2rem]'>{weight}</span>
+
+			</div>
+			<div className='p-8 '>
+				<h2 className='text-xl font-semibold'>{title}</h2>
+				<p className='mb-8 '>{description}</p>
+				<span className='text-xl font-semibold'>{price}</span>
+			</div>
+		</div>
+	)
+}
+
+export default MenuItem
---
+++ src/modules/menu/menu-item/menu-item.story.tsx
@@ -0,0 +1,21 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import MenuItem from './menu-item.component'
+
+
+export default {
+	title: 'Menu/Menu Item',
+	component: MenuItem
+} as ComponentMeta<typeof MenuItem>
+
+const Template: ComponentStory<typeof MenuItem> = (args) => <MenuItem{...args} />
+
+export const View = Template.bind({})
+View.args = {
+
+	imagePath: '/aseets/burger.jpeg',
+	weight: 555,
+	title: 'Title',
+	description: 'Description',
+	price: 131
+
+}
